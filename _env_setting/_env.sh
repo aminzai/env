dcf10881c948ffc686fe5484007c2974fb2b1128
Amin Wang <lagunawang@gmail.com>
2021-09-15 16:59:07 +0800

    add ls FreeBSD setting
    Signed-off-by: Kang-min Wang (Aminzai) <lagunawang@gmail.com>

--- _env_setting/_env.sh
+++ _env_setting/_env.sh
@@ -26,16 +26,21 @@ alias g='gvim'
 
 # enable color support of ls and also add handy aliases
 # NOTE: MacOSX not support parameter '--color=auto'
-if [ `uname` = "Darwin" ];
-then
-    alias ls='ls -G'
-else
-    eval "`dircolors -b`"
-    alias ls='ls --color=auto'
-    alias dir='dir --color=auto'
-    alias vdir='vdir --color=auto'
+case `uname` in
+    "Darwin")
+        alias ls='ls -G'
+        ;;
+    "FreeBSD")
+        alias ls='ls -G'
+        ;;
+    *)
+        #eval "`dircolors -b`"
+        #alias ls='ls --color=auto'
+        alias dir='dir --color=auto'
+        alias vdir='vdir --color=auto'
 
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
+        alias grep='grep --color=auto'
+        alias fgrep='fgrep --color=auto'
+        alias egrep='egrep --color=auto'
+        ;;
+esac
